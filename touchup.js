2e47b18d5ef4fb476831540e6f69afc2ab74746a
deathcap <deathcap@gmx.co.uk>
2016-05-09 21:05:10 -0700

    Port to native ES6, remove CoffeeScript

--- touchup.js
+++ touchup.js
@@ -1,121 +1,137 @@
+'use strict';
 
-createCanvas = (w, h) ->
-  canvas = document.createElement 'canvas'
-  canvas.setAttribute 'width', w
-  canvas.setAttribute 'height', h
+function createCanvas(w, h) {
+  const canvas = document.createElement('canvas');
+  canvas.setAttribute('width', w);
+  canvas.setAttribute('height', h);
 
-  context = canvas.getContext '2d'
+  const context = canvas.getContext('2d');
 
-  return [canvas, context]
+  return [canvas, context];
+}
 
-repeat = (sourceImage, timesX, timesY) ->
-  destW = sourceImage.width * timesX
-  destH = sourceImage.height * timesY
+function repeat(sourceImage, timesX, timesY) {
+  const destW = sourceImage.width * timesX;
+  const destH = sourceImage.height * timesY;
 
-  [canvas, context] = createCanvas destW, destH
+  const [canvas, context] = createCanvas(destW, destH);
 
-  pattern = context.createPattern sourceImage, 'repeat'
+  const pattern = context.createPattern(sourceImage, 'repeat');
 
-  context.fillStyle = pattern
-  context.fillRect 0, 0, destW, destH
+  context.fillStyle = pattern;
+  context.fillRect(0, 0, destW, destH);
 
-  return canvas.toDataURL()
+  return canvas.toDataURL();
+}
 
-# make the image bigger or smaller
-# note from spec http://www.w3.org/html/wg/drafts/2dcontext/html5_canvas_CR/#drawing-images-to-the-canvas
-# "This specification does not define the algorithm to use when scaling the image, if necessary."
-# which makes it mostly useless
-scale = (sourceImage, scaleX, scaleY, algorithm) ->
-  destW = sourceImage.width * scaleX
-  destH = sourceImage.width * scaleY
+// make the image bigger or smaller
+// note from spec http://www.w3.org/html/wg/drafts/2dcontext/html5_canvas_CR/#drawing-images-to-the-canvas
+// "This specification does not define the algorithm to use when scaling the image, if necessary."
+// which makes it mostly useless
+function scale(sourceImage, scaleX, scaleY, algorithm) {
+  const destW = sourceImage.width * scaleX;
+  const destH = sourceImage.width * scaleY;
 
-  [canvas, context] = createCanvas destW, destH
+  const [canvas, context] = createCanvas(destW, destH);
 
-  if algorithm == 'nearest-neighbor'
-    context.imageSmoothingEnabled = false
-    context.webkitImageSmoothingEnabled = false
-    context.mozImageSmoothingEnabled = false
-  # TODO: more algorithms, probably using ndarray see https://github.com/deathcap/touchup/issues/1
+  if (algorithm === 'nearest-neighbor') {
+    context.imageSmoothingEnabled = false;
+    context.webkitImageSmoothingEnabled = false;
+    context.mozImageSmoothingEnabled = false;
+  }
+  // TODO: more algorithms, probably using ndarray see https://github.com/deathcap/touchup/issues/1
 
-  context.drawImage sourceImage, 0, 0, destW, destH
+  context.drawImage(sourceImage, 0, 0, destW, destH);
 
-  return canvas.toDataURL()
+  return canvas.toDataURL();
+}
 
-crop = (sourceImage, ox, oy, ow, oh) ->
-  sx = ox || 0
-  sy = oy || 0
+function crop(sourceImage, ox, oy, ow, oh) {
+  const sx = ox || 0;
+  const sy = oy || 0;
 
-  destW = sourceImage.width - (ow || 0) - sx
-  destH = sourceImage.height - (oh || 0) - sy
+  const destW = sourceImage.width - (ow || 0) - sx;
+  const destH = sourceImage.height - (oh || 0) - sy;
 
-  sw = destW
-  sh = destH
+  const sw = destW;
+  const sh = destH;
 
-  [canvas, context] = createCanvas destW, destH
+  const [canvas, context] = createCanvas(destW, destH);
 
-  console.log(sx,sy,sw,sh,0,0,destW,destH)
-  context.drawImage sourceImage, sx, sy, sw, sh, 0, 0, destW, destH
+  console.log(sx,sy,sw,sh,0,0,destW,destH);
+  context.drawImage(sourceImage, sx, sy, sw, sh, 0, 0, destW, destH);
 
-  return canvas.toDataURL()
+  return canvas.toDataURL();
+}
 
-overallSize = (sourceImages) ->
-  destW = destH = 0
-  for sourceImage in sourceImages
-    destW = sourceImage.width if sourceImage.width > destW
-    destH = sourceImage.height if sourceImage.height > destH
+function overallSize(sourceImages) {
+  let destW = 0, destH = 0;
+  for (let sourceImage of sourceImages) {
+    if (sourceImage.width > destW) destW = sourceImage.width;
+    if (sourceImage.height > destH) destH = sourceImage.height;
+  }
 
-  return [destW, destH]
+  return [destW, destH];
+}
 
-overlay = (sourceImages, operation, alpha) ->
-  [destW, destH] = overallSize sourceImages
-  [canvas, context] = createCanvas destW, destH
+function overlay(sourceImages, operation, alpha) {
+  const [destW, destH] = overallSize(sourceImages);
+  const [canvas, context] = createCanvas(destW, destH);
 
-  # see http://www.w3.org/html/wg/drafts/2dcontext/html5_canvas_CR/#compositing
-  context.globalAlpha = alpha ? 1.0
-  context.globalCompositeOperation = operation ? 'source-over'
+  //  see http://www.w3.org/html/wg/drafts/2dcontext/html5_canvas_CR/#compositing
+  context.globalAlpha = alpha !== undefined ? alpha : 1.0;
+  context.globalCompositeOperation = operation !== undefined ? operation : 'source-over';
 
-  for sourceImage in sourceImages
-    context.drawImage sourceImage, 0, 0
+  for (let sourceImage of sourceImages) {
+    context.drawImage(sourceImage, 0, 0);
+  }
 
-  return canvas.toDataURL()
+  return canvas.toDataURL();
+}
 
-packRGBA = (r, g, b, a) ->
-  r * 0x01000000 +    # not << 24 because of signedness
+function packRGBA(r, g, b, a) {
+  return r * 0x01000000 +    // not << 24 because of signedness
     (g << 16) +
     (b << 8) +
-    a
+    a;
+}
 
-unpackRGBA = (n) ->
-  r = n >>> 24
-  g = n >>> 16 & 0xff
-  b = n >>> 8 & 0xff
-  a = n & 0xff
-  [r, g, b, a]
+function unpackRGBA(n) {
+  const r = n >>> 24;
+  const g = n >>> 16 & 0xff;
+  const b = n >>> 8 & 0xff;
+  const a = n & 0xff;
+  return [r, g, b, a];
+}
 
-recolor = (sourceImage, fromColor, toColor) ->
-  [canvas, context] = createCanvas sourceImage.width, sourceImage.height
+function recolor(sourceImage, fromColor, toColor) {
+  const [canvas, context] = createCanvas(sourceImage.width, sourceImage.height);
 
-  context.drawImage sourceImage, 0, 0
-  imagedata = context.getImageData 0, 0, sourceImage.width, sourceImage.height
-  data = imagedata.data
-  for i in [0...data.length] by 4   # TODO: optimize, use ndarray/cwise?
-    #[r, g, b, a] = data.data[i..i + 3] # Uint8ClampedArray has no method 'slice'
-    r = data[i]
-    g = data[i + 1]
-    b = data[i + 2]
-    a = data[i + 3]
-    rgba = packRGBA r, g, b, a
-    #console.log 'rgba',r,g,b,rgba,rgba.toString(16)
+  context.drawImage(sourceImage, 0, 0);
+  const imagedata = context.getImageData(0, 0, sourceImage.width, sourceImage.height);
+  const data = imagedata.data;
+  for (let i = 0; i < data.length; i += 4) { // TODO: optimize, use ndarray/cwise?
+    //[r, g, b, a] = data.data[i..i + 3] # Uint8ClampedArray has no method 'slice'
+    let r = data[i];
+    let g = data[i + 1];
+    let b = data[i + 2];
+    let a = data[i + 3];
+    let rgba = packRGBA(r, g, b, a);
+    //console.log('rgba',r,g,b,rgba,rgba.toString(16))
 
-    if rgba == fromColor
-      [r, g, b, a] = unpackRGBA toColor
-      data[i] = r
-      data[i + 1] = g
-      data[i + 2] = b
-      data[i + 3] = a
+    if (rgba === fromColor) {
+      [r, g, b, a] = unpackRGBA(toColor);
+      data[i] = r;
+      data[i + 1] = g;
+      data[i + 2] = b;
+      data[i + 3] = a;
+    }
+  }
 
-  context.putImageData imagedata, 0, 0
+  context.putImageData(imagedata, 0, 0);
 
-  return canvas.toDataURL()
+  return canvas.toDataURL();
+}
 
-module.exports = { repeat, scale, crop, overlay, recolor }
+module.exports = {repeat, scale, crop, overlay, recolor };
+
